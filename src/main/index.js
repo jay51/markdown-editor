3cb674b41fba96eea161b3eaafaf8f3cf31dfbfd
jay51 <nenojaafar3@gmail.com>
2019-02-08 15:12:07 -0600

    installed a markdown parser and implmented the basic react Text input things

--- src/main/index.js
+++ src/main/index.js
@@ -1,62 +1,106 @@
-'use strict'
+"use strict";
 
-import { app, BrowserWindow } from 'electron'
-import * as path from 'path'
-import { format as formatUrl } from 'url'
+import { app, BrowserWindow, Menu, shell, dialog } from "electron";
+import * as path from "path";
+import { format as formatUrl } from "url";
 
-const isDevelopment = process.env.NODE_ENV !== 'production'
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 // global reference to mainWindow (necessary to prevent window from being garbage collected)
-let mainWindow
+let mainWindow;
 
 function createMainWindow() {
-  const window = new BrowserWindow()
+	// set width and height for window
+	const window = new BrowserWindow({ width: 999, height: 650 });
 
-  if (isDevelopment) {
-    window.webContents.openDevTools()
-  }
+	if (isDevelopment) {
+		window.webContents.openDevTools();
+	}
+	// url for static files
+	if (isDevelopment) {
+		window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
+	} else {
+		// load the static files(html, css, js)
+		window.loadURL(
+			formatUrl({
+				pathname: path.join(__dirname, "index.html"),
+				protocol: "file",
+				slashes: true
+			})
+		);
+	}
 
-  if (isDevelopment) {
-    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
-  }
-  else {
-    window.loadURL(formatUrl({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file',
-      slashes: true
-    }))
-  }
+	window.on("closed", () => {
+		mainWindow = null;
+	});
 
-  window.on('closed', () => {
-    mainWindow = null
-  })
+	window.webContents.on("devtools-opened", () => {
+		window.focus();
+		setImmediate(() => {
+			window.focus();
+		});
+	});
 
-  window.webContents.on('devtools-opened', () => {
-    window.focus()
-    setImmediate(() => {
-      window.focus()
-    })
-  })
-
-  return window
+	return window;
 }
 
 // quit application when all windows are closed
-app.on('window-all-closed', () => {
-  // on macOS it is common for applications to stay open until the user explicitly quits
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on("window-all-closed", () => {
+	// on macOS it is common for applications to stay open until the user explicitly quits
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
 
-app.on('activate', () => {
-  // on macOS it is common to re-create a window even after all windows have been closed
-  if (mainWindow === null) {
-    mainWindow = createMainWindow()
-  }
-})
+app.on("activate", () => {
+	// on macOS it is common to re-create a window even after all windows have been closed
+	if (mainWindow === null) {
+		mainWindow = createMainWindow();
+	}
+});
+
+let template = [
+	{
+		//The top level label/button
+		label: "Edit",
+		submenu: [
+			{
+				// options inside the label
+				label: "Undo",
+				// keyboard shortcut to run the Undo option
+				accelerator: "CmdorCtrl+z",
+				role: "undo"
+			}
+		]
+	},
+	{
+		label: "View",
+		submenu: [
+			{
+				label: "demo",
+				// click event
+				click: function(item, focusWindow) {
+					console.log("Show dialog box");
+					let options = {
+						type: "info",
+						title: "App Demo",
+						buttons: ["Ok"],
+						message: "You clicked The menu and Demo option"
+					};
+					dialog.showMessageBox(focusWindow, options, function(res, checked) {
+						console.log(res);
+						console.log(checked);
+					});
+				}
+			}
+		]
+	}
+];
 
 // create main BrowserWindow when electron is ready
-app.on('ready', () => {
-  mainWindow = createMainWindow()
-})
+app.on("ready", () => {
+	mainWindow = createMainWindow();
+
+	const menu = Menu.buildFromTemplate(template);
+	Menu.setApplicationMenu(menu);
+});
--- src/renderer/app.js
+++ src/renderer/app.js
@@ -2,8 +2,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
+import showdown from "showdown";
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			"text-editor": "",
+			parseData: ""
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.parseTextToMarkdown = this.parseTextToMarkdown.bind(this);
+		this.converter = new showdown.Converter();
+	}
+
+	handleChange(e) {
+		const text = e.target.value;
+		this.setState({ [e.target.name]: text });
+		this.parseTextToMarkdown(text);
+	}
+
+	parseTextToMarkdown(text) {
+		// parse data to markdown
+		console.log("parseData");
+		text = this.converter.makeHtml(text);
+		// setState for parsedData
+		this.setState({ parseData: text });
+	}
+
 	render() {
 		return (
 			<section className="container">
@@ -19,43 +45,15 @@ class App extends React.Component {
 				</div>
 				{/* first editor for markdown text */}
 				<div className="editor">
-					<textarea name="text-editor" id="text-editor" />
+					<textarea
+						name="text-editor"
+						value={this.state["text-editor"]}
+						onChange={this.handleChange}
+						id="text-editor"
+					/>
 				</div>
 				{/* second editor for formated text */}
-				<div className="editor md">
-					<h1>second</h1>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum
-					possimus deserunt, quo maiores repellendus dolore facere quaerat quod
-					nisi perspiciatis nihil iste neque ratione iusto voluptatem? Dicta
-					deleniti adipisci, aut cupiditate natus asperiores magnam blanditiis
-					fugit nobis obcaecati unde in, quo aspernatur ab inventore illo porro.
-					Quis sunt et voluptates amet repudiandae! Dolore dolorem aliquam eaque
-					necessitatibus optio, laborum sequi fugit assumenda est ipsum illum
-					cupiditate sit exercitationem qui tenetur tempore? Ipsam fugiat atque
-					nemo, sequi maxime, nostrum totam consectetur eum aperiam nobis,
-					perspiciatis tenetur. Asperiores cupiditate praesentium quisquam
-					assumenda?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
-					rem quisquam, cupiditate culpa est soluta itaque voluptas? Aspernatur,
-					minus soluta magnam necessitatibus debitis fugit suscipit repellat,
-					neque quibusdam, officiis nam adipisci fugiat modi quos corrupti
-					nesciunt similique cumque. Dolore labore molestias amet blanditiis,
-					deleniti voluptatum enim cupiditate quos. Corrupti natus deleniti
-					quaerat dolorum dolorem eveniet. Laborum voluptate deserunt
-					dignissimos, in praesentium error neque cupiditate officia?
-					Necessitatibus amet expedita, consectetur minima, cumque magni
-					recusandae impedit tempora eaque veritatis blanditiis aut! Neque
-					soluta amet dignissimos ratione, consequatur non. Minus molestiae modi
-					neque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
-					dolores explicabo pariatur ea nisi enim ipsam in ipsa perferendis ex
-					ullam modi, sint minima corporis maxime dolore beatae impedit eligendi
-					numquam provident, suscipit ut. Magnam placeat voluptatem laudantium
-					ad repudiandae quaerat aut dignissimos, expedita architecto iure, nisi
-					nulla culpa deleniti velit perferendis mollitia voluptas doloremque
-					tempora fugiat, ipsum provident numquam? Officia, ut facilis? Odit
-					atque beatae consequatur delectus! Repellendus magnam harum numquam
-					delectus amet ducimus sequi atque cupiditate. Quasi odit, tempore
-					inventore sunt repellat modi neque vel molestias eos et.
-				</div>
+				<div className="editor md">{this.state.parseData}</div>
 			</section>
 		);
 	}
